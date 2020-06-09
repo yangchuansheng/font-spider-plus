249de1425dba778a177eda626067eb6e6ba410f0
Ryan Yang <yangchuansheng33@gmail.com>
2020-06-09 14:53:15 +0800

    :art: 修改脚本

--- example/start.sh
+++ example/start.sh
@@ -7,26 +7,18 @@ sed "s/\$font/$font/g" index/index.html.base > index/index.html
 
 fsp local index/index.html
 
-eot=$(cat fonts/$font.eot|base64|tr -d '\n')
-woff=$(cat fonts/$font.woff|base64|tr -d '\n')
+# eot=$(cat fonts/$font.eot|base64|tr -d '\n')
+# woff=$(cat fonts/$font.woff|base64|tr -d '\n')
 woff2=$(cat fonts/$font.woff2|base64|tr -d '\n')
-ttf=$(cat fonts/$font.ttf|base64|tr -d '\n')
-svg=$(cat fonts/$font.svg|base64|tr -d '\n')
+# ttf=$(cat fonts/$font.ttf|base64|tr -d '\n')
+# svg=$(cat fonts/$font.svg|base64|tr -d '\n')
 
 cat > fonts-zh.css <<EOF
 @font-face {
     font-family: '$font';
-    src: url(data:application/font-eot;charset=utf-8;base64,$eot) format('eot');
+    src: url(data:application/font-woff2;charset=utf-8;base64,$woff2) format('woff2');
     font-weight: normal;
     font-style: normal;
-}
-@font-face {
-    font-family: '$font';
-    src: url(data:application/font-woff2;charset=utf-8;base64,$woff2) format('woff2'),
-         url(data:application/font-woff;charset=utf-8;base64,$woff) format('woff'),
-	 url(data:application/font-ttf;charset=utf-8;base64,$ttf) format('truetype'),
-	 url(data:application/font-svg;charset=utf-8;base64,$svg) format('svg');
-    font-weight: normal;
-    font-style: normal;
+    font-display: swap;
 }
 EOF
